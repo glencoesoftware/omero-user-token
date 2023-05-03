b4a6921c5b587d2c11b7eadb6fe13e2184b75428
Sébastien Besson <sbesson@glencoesoftware.com>
2023-05-03 11:49:42 +0100

    Add additional classifiers

--- setup.py
+++ setup.py
@@ -51,7 +51,15 @@ setup(name='omero-user-token',
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
       classifiers=[
+          'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: GNU General Public License v2 (GPLv2)'
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10'
       ],
       keywords='',
       author='Glencoe Software, Inc.',
